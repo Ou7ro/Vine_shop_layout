5028e6da67d003177a5741fe196175fdafdac1fd
Chepik Pavel <126232792+Ou7ro@users.noreply.github.com>
2025-03-28 11:21:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,21 @@ import collections
 import argparse
 
 
-def get_year_word():
-    year = calculating_age_company()
-    if year % 100 in (11, 12, 13, 14):
-        return f'{year} лет'
-    last_digit = year % 10
+def get_year_word(company_age):
+    if company_age % 100 in (11, 12, 13, 14):
+        return f'{company_age} лет'
+    last_digit = company_age % 10
     if last_digit == 1:
-        return f'{year} год'
+        return f'{company_age} год'
     if 2 <= last_digit <= 4:
-        return f'{year} года'
-    return f'{year} лет'
+        return f'{company_age} года'
+    return f'{company_age} лет'
 
 
-def calculating_age_company():
+def calculate_age_company():
     get_startet = 1920
-    difference = datetime.now().year - get_startet
-    return difference
+    company_age = datetime.now().year - get_startet
+    return company_age
 
 
 def main():
@@ -33,12 +32,12 @@ def main():
     parser = argparse.ArgumentParser(description='Введите путь до файла, либо же название файла, если он уже находится в директории скрипта')
     parser.add_argument('--path', default=excel_path, help='Путь')
     args = parser.parse_args()
+    
+    wine_table = pandas.read_excel(args.path).fillna('')
 
-    excel_data_wine2 = pandas.read_excel(args.path).fillna('')
+    wines_by_category = collections.defaultdict(list)
 
-    wines_dict = collections.defaultdict(list)
-
-    for _, row in excel_data_wine2.iterrows():
+    for _, row in wine_table.iterrows():
         category = row['Категория']
         wine_data = {
             'Картинка': row['Картинка'],
@@ -48,17 +47,19 @@ def main():
             'Цена': row['Цена'],
             'Акция': row['Акция']
         }
-        wines_dict[category].append(wine_data)
+        wines_by_category[category].append(wine_data)
 
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
     )
     template = env.get_template('template.html')
-
+    
+    company_age = calculate_age_company()
+    
     rendered_page = template.render(
-        age_text=f'Уже {get_year_word()} с вами',
-        wines_dict=wines_dict
+        age_text=f'Уже {get_year_word(company_age)} с вами',
+        wines_dict=wines_by_category
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
